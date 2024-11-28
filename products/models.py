ba05eb739689975cd1c81733de2e75f68c092e23
Esau Jesse <jesseesau13@gmail.com>
2024-11-28 06:09:07 +0100

    Added Products model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2)  # Max price: 99999999.99
+    category = models.CharField(max_length=255, blank=True, null=True)
+    stock_quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(
+        upload_to='product_images/', blank=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def reduce_stock(self, quantity):
+        """Reduce stock when an order is placed."""
+        if self.stock_quantity >= quantity:
+            self.stock_quantity -= quantity
+            self.save()
+        else:
+            raise ValueError("Out of stock stock!")
